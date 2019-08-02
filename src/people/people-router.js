a9d09140e2d43e4cad281ee275b20e29a6d73d0c
michaelbravo777 <michael.bravo777@gmail.com>
2019-08-02 16:17:43 -0400

    added post to peopleRouter

--- src/people/people-router.js
+++ src/people/people-router.js
@@ -2,10 +2,16 @@ const express = require('express')
 const store = require('../store')
 const peopleRouter = express.Router()
 
+const jsonBodyParser = express.json()
+
 peopleRouter
   .route('/')
   .get((req, res) => {
     res.json(store.people.display())
+  })
+  .post(jsonBodyParser, (req, res) => {
+    store.people.enqueue(req.body.name)
+    res.json(store.people.peek())
   })
 
 module.exports = peopleRouter
