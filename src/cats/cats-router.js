6ed03abccb59412614f840c92750f87c99a08db9
michaelbravo777 <michael.bravo777@gmail.com>
2019-08-02 16:01:43 -0400

    added store

--- src/cats/cats-router.js
+++ src/cats/cats-router.js
@@ -5,6 +5,9 @@ const catsRouter = express.Router()
 catsRouter
   .route('/')
   .get((req, res) => {
+    store.history.enqueue({animal: store.cats.peek(), person: store.people.peek()})
+    store.people.dequeue()
+    store.cats.dequeue()
     res.json(store.cats.peek())
   })
 
